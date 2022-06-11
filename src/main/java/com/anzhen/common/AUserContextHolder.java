25dacfe0dcea7091dc7ea69d37806e0df1336ea1
AnZhen <599002514@qq.com>
2022-06-11 21:26:41 +0800

    fix 解决登录方式提供者报错问题

--- src/main/java/com/anzhen/common/AUserContextHolder.java
+++ src/main/java/com/anzhen/common/AUserContextHolder.java
@@ -1,12 +1,14 @@
 package com.anzhen.common;
 
+/**
+ * 自定义上下文对象
+ */
 public class AUserContextHolder {
     static ThreadLocal<AUserContext> userContextHolderThreadLocal = new ThreadLocal<>();
 
     public static AUserContext getAUserContext() {
         return userContextHolderThreadLocal.get();
     }
-
 
     public static void setAUserContext(AUserContext aUserContext) {
         userContextHolderThreadLocal.set(aUserContext);
--- src/main/java/com/anzhen/config/oauth2/WebSecurityConfig.java
+++ src/main/java/com/anzhen/config/oauth2/WebSecurityConfig.java
@@ -4,21 +4,26 @@ import com.anzhen.config.oauth2.github.GitHubAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Resource
+    private UserDetailsService userDetailsService;
     /**
      * 认证管理对象
      *
@@ -59,6 +64,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(gitHubAuthenticationProvider());
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    /**
+     * 用户名密码认证授权提供者
+     *
+     * @return
+     */
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setHideUserNotFoundExceptions(false); // 是否隐藏用户不存在异常，默认:true-隐藏；false-抛出异常；
+        return provider;
     }
 
     /**
