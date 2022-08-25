81ae8f849662b8c06c522304ae63ffb651d5e656
AnZhen <599002514@qq.com>
2022-08-25 11:27:17 +0800

    fix 删除启动类上没用的类

--- src/main/java/com/anzhen/ImagesApplication.java
+++ src/main/java/com/anzhen/ImagesApplication.java
@@ -2,8 +2,6 @@ package com.anzhen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author AnZhen
