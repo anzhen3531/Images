b4e415430c065d2a7f799a2cf5d48a275eac0aa9
AnZhen <599002514@qq.com>
2022-08-25 11:21:59 +0800

    feat 爬取图片采用异步方式

--- src/main/java/com/anzhen/jsoup/AdvanceImage.java
+++ src/main/java/com/anzhen/jsoup/AdvanceImage.java
@@ -1,16 +1,5 @@
 package com.anzhen.jsoup;
 
-import com.anzhen.service.AImageService;
-import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.helper.StringUtil;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
@@ -23,206 +12,225 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.Resource;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.StringUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.anzhen.service.AImageService;
+
+import lombok.extern.slf4j.Slf4j;
+
 /** 这个只是针对首页 */
 @Component
 @Slf4j
+@EnableScheduling
+@EnableAsync
 public class AdvanceImage {
 
-  /** 爬取的网站基础前缀 */
-  static final String basePath = "https://wallhaven.cc/hot?page=";
+    /** 爬取的网站基础前缀 */
+    static final String basePath = "https://wallhaven.cc/hot?page=";
 
-  /** 创建默认大小的缓冲区 */
-  static ByteBuffer buffer = ByteBuffer.allocate(1024);
+    /** 创建默认大小的缓冲区 */
+    static ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-  @Resource AImageService aImageService;
+    @Resource
+    AImageService aImageService;
 
-  /** 封装请求头 */
-  static HashMap<String, String> headers = new HashMap<>();
+    /** 封装请求头 */
+    static HashMap<String, String> headers = new HashMap<>();
 
-  /** 文档对象 */
-  static Document document;
+    /** 文档对象 */
+    static Document document;
 
-  static {
-    // 封装请求头
-    headers.put("Referer", "https://wallhaven.cc/");
-    headers.put(
-        "sec-ch-ua",
-        "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"");
-    headers.put("sec-ch-ua-mobile", "?0");
-    headers.put("sec-ch-ua-platform", "Windows");
-    headers.put(
-        "User-Agent",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
-  }
+    static {
+        // 封装请求头
+        headers.put("Referer", "https://wallhaven.cc/");
+        headers.put("sec-ch-ua", "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"");
+        headers.put("sec-ch-ua-mobile", "?0");
+        headers.put("sec-ch-ua-platform", "Windows");
+        headers.put("User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
+    }
 
-  /** 开始爬虫任务 */
-  @Scheduled(cron = "0 0 18 * * ?")
-  public void scheduledTask() {
-    log.info("定时任务执行");
-    try {
-      getThumbnail(basePath + 1);
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      log.info("由于出现了读取失败的问题 尝试重新运行定时任务  第一次尝试");
-      try {
-        getThumbnail(basePath + 1);
-      } catch (Exception ex) {
-        log.info("由于出现了读取失败的问题 尝试重新运行定时任务  第二次尝试");
+    /**
+     * 开启定时任务 定时爬取图片 将这个定时任务改成定时任务+异步任务
+     */
+    @Scheduled(cron = "0 0 18 * * ?")
+    @Async
+    public void scheduledTask() {
+        log.info("定时任务执行");
         try {
-          getThumbnail(basePath + 1);
-        } catch (Exception ee) {
-          log.error(ee.getMessage());
+            getThumbnail(basePath + 1);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            log.info("由于出现了读取失败的问题 尝试重新运行定时任务  第一次尝试");
+            try {
+                getThumbnail(basePath + 1);
+            } catch (Exception ex) {
+                log.info("由于出现了读取失败的问题 尝试重新运行定时任务  第二次尝试");
+                try {
+                    getThumbnail(basePath + 1);
+                } catch (Exception ee) {
+                    log.error(ee.getMessage());
+                }
+            }
         }
-      }
     }
-  }
 
-  /**
-   * 获取缩略图List
-   *
-   * @param path : 文件路径
-   * @throws Exception
-   */
-  public void getThumbnail(String path) throws Exception {
-    document = Jsoup.connect(path).headers(headers).get();
-    log.info("解析文件成功");
-    // 获取首页标签的缩略图
-    List<String> tagThumbnail = getIndexImage("thumb-listing-page");
-    log.info("获取图片选择器成功");
-    for (String s : tagThumbnail) {
-      // 代码优化 使用异步写入文件
-      List<String> photoPath = getPhotoPath(Jsoup.connect(s).headers(headers).get());
-      System.out.println(photoPath);
-      // 写入本地文件
-      // 可以采用线程池
-      for (String s1 : photoPath) {
-        writePhotoByMinIO(s1);
-      }
-    }
-  }
-
-  /**
-   * 通过类选择器获取首页的照片粗略图路径
-   *
-   * @param className 类选择器名称
-   */
-  public List<String> getIndexImage(String className) {
-    List<String> hrefs = new ArrayList<>();
-    Elements elements = document.getElementsByClass(className);
-    for (Element element : elements) {
-      Elements li = element.getElementsByTag("li");
-      for (Element element1 : li) {
-        // 获取到A标签之后的内容 保存缩略图
-        Elements a = element1.getElementsByTag("a");
-        for (Element element2 : a) {
-          String href = element2.attr("href");
-          if (!href.isBlank() && !href.isEmpty()) hrefs.add(href);
+    /**
+     * 获取缩略图List
+     *
+     * @param path : 文件路径
+     */
+    public void getThumbnail(String path) throws Exception {
+        document = Jsoup.connect(path).headers(headers).get();
+        log.info("解析文件成功");
+        // 获取首页标签的缩略图
+        List<String> tagThumbnail = getIndexImage("thumb-listing-page");
+        log.info("获取图片选择器成功");
+        for (String s : tagThumbnail) {
+            // 代码优化 使用异步写入文件
+            List<String> photoPath = getPhotoPath(Jsoup.connect(s).headers(headers).get());
+            System.out.println(photoPath);
+            // 写入本地文件
+            // 可以采用线程池
+            for (String s1 : photoPath) {
+                writePhotoByMinIO(s1);
+            }
         }
-      }
     }
-    return hrefs;
-  }
 
-  /**
-   * 解析不同标签中的缩略图路径
-   *
-   * @param className 标签的class属性名
-   */
-  public List<String> getTagThumbnail(String className) {
-    List<String> hrefs = new ArrayList<>();
-    Elements elements = document.getElementsByClass(className);
-    for (Element element : elements) {
-      Elements a = element.getElementsByTag("a");
-      for (Element element1 : a) {
-        String href = element1.attr("href");
-        hrefs.add(href);
-      }
+    /**
+     * 通过类选择器获取首页的照片粗略图路径
+     *
+     * @param className 类选择器名称
+     */
+    public List<String> getIndexImage(String className) {
+        List<String> hrefs = new ArrayList<>();
+        Elements elements = document.getElementsByClass(className);
+        for (Element element : elements) {
+            Elements li = element.getElementsByTag("li");
+            for (Element element1 : li) {
+                // 获取到A标签之后的内容 保存缩略图
+                Elements a = element1.getElementsByTag("a");
+                for (Element element2 : a) {
+                    String href = element2.attr("href");
+                    if (!href.isBlank() && !href.isEmpty())
+                        hrefs.add(href);
+                }
+            }
+        }
+        return hrefs;
     }
-    return hrefs;
-  }
 
-  /** 遍历缩略图路径 获取大图路径 */
-  public List<String> getPhotoPath(Document document) {
-    List<String> list = new ArrayList<>();
-    Elements scrollbox = document.getElementsByClass("scrollbox");
-    for (Element element : scrollbox) {
-      Elements img = element.getElementsByTag("img");
-      for (Element path : img) {
-        String src = path.attr("src");
-        list.add(src);
-      }
+    /**
+     * 解析不同标签中的缩略图路径
+     *
+     * @param className 标签的class属性名
+     */
+    public List<String> getTagThumbnail(String className) {
+        List<String> hrefs = new ArrayList<>();
+        Elements elements = document.getElementsByClass(className);
+        for (Element element : elements) {
+            Elements a = element.getElementsByTag("a");
+            for (Element element1 : a) {
+                String href = element1.attr("href");
+                hrefs.add(href);
+            }
+        }
+        return hrefs;
     }
-    return list;
-  }
 
-  /** 写入到本地文件 */
-  public void writePhoto(String path) throws Exception {
-    if (StringUtil.isBlank(path)) {
-      return;
+    /** 遍历缩略图路径 获取大图路径 */
+    public List<String> getPhotoPath(Document document) {
+        List<String> list = new ArrayList<>();
+        Elements scrollbox = document.getElementsByClass("scrollbox");
+        for (Element element : scrollbox) {
+            Elements img = element.getElementsByTag("img");
+            for (Element path : img) {
+                String src = path.attr("src");
+                list.add(src);
+            }
+        }
+        return list;
     }
-    long startTime = System.currentTimeMillis();
-    // 打开url 流
-    InputStream inputStream = new URL(path).openStream();
-    try (FileOutputStream fileOutputStream =
-        new FileOutputStream("D:\\Files\\" + UUID.randomUUID() + ".jpg")) {
-      int temp;
-      while ((temp = inputStream.read()) != -1) fileOutputStream.write(temp);
-    }
-    long endTime = System.currentTimeMillis();
-    System.out.println("写入完成 ！！！！");
-    System.out.println("耗时为:" + (endTime - startTime) / 1000);
-  }
 
-  /**
-   * 使用NIO写入本地文件
-   *
-   * @param path : 文件路径
-   */
-  public void writeNioPhoto(String path) throws Exception {
-    if (StringUtil.isBlank(path)) {
-      return;
+    /** 写入到本地文件 */
+    public void writePhoto(String path) throws Exception {
+        if (StringUtil.isBlank(path)) {
+            return;
+        }
+        long startTime = System.currentTimeMillis();
+        // 打开url 流
+        InputStream inputStream = new URL(path).openStream();
+        try (FileOutputStream fileOutputStream = new FileOutputStream("D:\\Files\\" + UUID.randomUUID() + ".jpg")) {
+            int temp;
+            while ((temp = inputStream.read()) != -1)
+                fileOutputStream.write(temp);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("写入完成 ！！！！");
+        System.out.println("耗时为:" + (endTime - startTime) / 1000);
     }
-    long startTime = System.currentTimeMillis();
-    InputStream inputStream = new URL(path).openStream();
-    RandomAccessFile rw = new RandomAccessFile("D:\\Files\\" + UUID.randomUUID() + ".jpg", "rw");
-    // 打开FileChannel
-    FileChannel channel = rw.getChannel();
 
-    byte[] readAllBytes = inputStream.readAllBytes();
-    // 通过判断来确定开辟的区域是否大于当前图片的大小
-    if (readAllBytes.length > buffer.capacity()) {
-      buffer = ByteBuffer.allocate(readAllBytes.length);
-    }
-    buffer.put(readAllBytes);
-    // 初始化缓冲区位置
-    buffer.flip();
-    // 写入操作
-    while (buffer.hasRemaining()) {
-      channel.write(buffer);
-    }
-    // 关闭流
-    channel.close();
-    inputStream.close();
-    buffer.clear();
-    long endTime = System.currentTimeMillis();
-    log.info("写入完成 ！！！！");
-    log.info("耗时为:" + (endTime - startTime) / 1000);
-  }
+    /**
+     * 使用NIO写入本地文件
+     *
+     * @param path : 文件路径
+     */
+    public void writeNioPhoto(String path) throws Exception {
+        if (StringUtil.isBlank(path)) {
+            return;
+        }
+        long startTime = System.currentTimeMillis();
+        InputStream inputStream = new URL(path).openStream();
+        RandomAccessFile rw = new RandomAccessFile("D:\\Files\\" + UUID.randomUUID() + ".jpg", "rw");
+        // 打开FileChannel
+        FileChannel channel = rw.getChannel();
 
-  /** 通过MinIO接口进行上传 */
-  public void writePhotoByMinIO(String path) throws Exception {
-    if (StringUtil.isBlank(path)) {
-      return;
+        byte[] readAllBytes = inputStream.readAllBytes();
+        // 通过判断来确定开辟的区域是否大于当前图片的大小
+        if (readAllBytes.length > buffer.capacity()) {
+            buffer = ByteBuffer.allocate(readAllBytes.length);
+        }
+        buffer.put(readAllBytes);
+        // 初始化缓冲区位置
+        buffer.flip();
+        // 写入操作
+        while (buffer.hasRemaining()) {
+            channel.write(buffer);
+        }
+        // 关闭流
+        channel.close();
+        inputStream.close();
+        buffer.clear();
+        long endTime = System.currentTimeMillis();
+        log.info("写入完成 ！！！！");
+        log.info("耗时为:" + (endTime - startTime) / 1000);
     }
-    long startTime = System.currentTimeMillis();
-    // 打开url 流
-    URLConnection urlConnection = new URL(path).openConnection();
-    InputStream inputStream = urlConnection.getInputStream();
-    String suffix = path.substring(path.lastIndexOf("."));
-    // 使用流去写入文件
-    aImageService.uploadFileAndDb(
-        inputStream, UUID.randomUUID() + suffix, urlConnection.getContentLength());
-    log.info("写入完成 ！！！！");
-    log.info("耗时为:" + (System.currentTimeMillis() - startTime) / 1000);
-  }
+
+    /** 通过MinIO接口进行上传 */
+    public void writePhotoByMinIO(String path) throws Exception {
+        if (StringUtil.isBlank(path)) {
+            return;
+        }
+        long startTime = System.currentTimeMillis();
+        // 打开url 流
+        URLConnection urlConnection = new URL(path).openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+        String suffix = path.substring(path.lastIndexOf("."));
+        // 使用流去写入文件
+        aImageService.uploadFileAndDb(inputStream, UUID.randomUUID() + suffix, urlConnection.getContentLength());
+        log.info("写入完成 ！！！！");
+        log.info("耗时为:" + (System.currentTimeMillis() - startTime) / 1000);
+    }
 }
